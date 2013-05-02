53133f81485654c222bc62d2d8ad31af69be1a3f
Matthew Rothenberg <mrothenberg@gmail.com>
2013-05-02 11:57:14 -0400

    exclude networked tests for speed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,10 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
+  # exclude networked tests temporarily for since they are slow
+  # later pop in VCR if needed
+  config.filter_run_excluding :networked => true
+
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
