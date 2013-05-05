66a87f4c72bbe842ff2beb33ab35810f6b0115d5
Matthew Rothenberg <mrothenberg@gmail.com>
2013-05-04 23:11:47 -0400

    some placeholder

--- lib/meatballtracker.rb
+++ lib/meatballtracker.rb
@@ -1,6 +1,6 @@
-require "meatballtracker/tracker"
-require "meatballtracker/menu_parser"
-require "meatballtracker/tweeter"
+require_relative "meatballtracker/tracker"
+require_relative "meatballtracker/menu_parser"
+require_relative "meatballtracker/tweeter"
 
 module Meatballtracker
 end
--- meatballtracker_main.rb
+++ meatballtracker_main.rb
@@ -1,3 +1,13 @@
+require './lib/meatballtracker'
+include Meatballtracker
+
+#temporary until we put the real logic in
+tracker = Tracker.new
+mp = MenuParser.new( tracker.current_menu_url )
+puts "Menu for #{mp.menu_date} #{mp.is_delicious? ? "does":"does not"} contain meatballs."
+if mp.is_delicious?
+  puts "The delicious menu item is: #{mp.delicious_item}"
+end
 # pseudo logic
 #  init a Tracker
 #  ask Tracker if there are new updates
