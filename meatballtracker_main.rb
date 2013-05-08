74ab5f917970c5844b42b5bd6da55aa7300ee78d
Matthew Rothenberg <mrothenberg@gmail.com>
2013-05-07 23:18:09 -0400

    stub some more core logic

--- meatballtracker_main.rb
+++ meatballtracker_main.rb
@@ -2,9 +2,16 @@ require './lib/meatballtracker'
 include Meatballtracker
 
 #temporary until we put the real logic in
-tracker = Tracker.new
-mp = MenuParser.new( tracker.current_menu_url )
-puts Tweeter.new_from_menu(mp).format_str
+@tracker = Tracker.new
+puts "Checking for new menu..."
+previous_url = Tweeter.most_recent_posted_menu_url
+if (@tracker.current_menu_url == previous_url)
+  puts "Menu is still same as previous menu."
+else
+  puts "Menu appears to be new!"
+  mp = MenuParser.new( @tracker.current_menu_url )
+  puts TweetFormatter.new_from_menu(mp).format_str
+end
 # pseudo logic
 #  init a Tracker
 #  ask Tracker if there are new updates
